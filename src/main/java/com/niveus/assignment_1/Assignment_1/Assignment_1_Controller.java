03e4bec53517b1963a88268389165085dff88163
pratheekkniveus <pratheek.k@niveussolutions.com>
2024-03-20 18:34:39 +0530

    First commit by PK

---
+++ src/main/java/com/niveus/assignment_1/Assignment_1/Assignment_1_Controller.java
@@ -0,0 +1,16 @@
+package com.niveus.assignment_1.Assignment_1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("run")
+public class Assignment_1_Controller {
+
+    @GetMapping("assignment1")
+    public String getMessage(){
+        return "This is a simple rest api for assignment 1";
+    }
+
+}
